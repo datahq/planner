930144480d46c90d0e9250698c37c67945ae9698
zelima <irakli.mchedlishvili@datopian.com>
2017-11-27 16:02:16 +0400

    [data validatuion][s]: simple add validate_resource processor -refs #7

--- planner/nodes/report_nodes.py
+++ planner/nodes/report_nodes.py
@@ -10,29 +10,13 @@ class ReportProcessingNode(BaseProcessingNode):
     def get_artifacts(self):
         datahub_type = 'derived/report'
         resource_name = 'datapackage_report'
-        out_path = '{}.{}'.format(resource_name, self.fmt)
         tabular_artifacts = [
             artifact for artifact in self.available_artifacts
-                if artifact.datahub_type == 'source/tabular'
+            if artifact.datahub_type == 'source/tabular'
         ]
         output = ProcessingArtifact(
             datahub_type, resource_name,
             tabular_artifacts, [],
-            [('assembler.validate_resource', {}),
-             ('dump.to_path', {
-                'out-path': out_path,
-                'force-format': False,
-                'handle-non-tabular': True
-            }),
-             ('add_resource', {
-                'url': out_path,
-                'name': resource_name,
-                'format': 'json',
-                'path': 'data/{}.json'.format(resource_name),
-                'datahub': {
-                  'type': "derived/report",
-                },
-                'description': 'Validation report for tabular data'
-             })],
+            [('assembler.validate_resource', {})],
             False)
         yield output
